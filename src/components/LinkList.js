d322945715d3ac05c84a2b8fac5543c8792b7519
allthatjazzleo <pangleo1994@gmail.com>
2019-10-08 22:43:53 +0800

    add filter subscription pagination

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,44 @@
 /* eslint-disable no-lone-blocks */
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import { LINKS_PER_PAGE } from '../constants'
 
 export const FEED_QUERY = gql`
-{
-  feed {
-    links {
-      id
-      createdAt
-      url
-      description
-      postedBy {
+  query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(first: $first, skip: $skip, orderBy: $orderBy) {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
           id
           name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      count
+    }
+  }
+`
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
       }
       votes {
         id
@@ -24,7 +48,33 @@ export const FEED_QUERY = gql`
       }
     }
   }
-}
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
 `
 class LinkList extends Component {
   
@@ -51,25 +101,101 @@ class LinkList extends Component {
   //   )
   // }
 
+  _getQueryVariables = () => {
+    const isNewPage = this.props.location.pathname.includes('new')
+    const page = parseInt(this.props.match.params.page, 10)
+  
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    return { first, skip, orderBy }
+  }
+
   _updateCacheAfterVote = (store, createVote, linkId) => {
-    try {
-    const data = store.readQuery({ query: FEED_QUERY, variables:{}})
-    console.log(data)
+    const isNewPage = this.props.location.pathname.includes('new')
+    const page = parseInt(this.props.match.params.page, 10)
+  
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    const data = store.readQuery({
+      query: FEED_QUERY,
+      variables: { first, skip, orderBy }
+    })
+  
     const votedLink = data.feed.links.find(link => link.id === linkId)
     votedLink.votes = createVote.link.votes
-  
     store.writeQuery({ query: FEED_QUERY, data })
-    } catch (e) {
-      console.log(e);
-      console.log("Not updating store - Projects not loaded yet");
+  }
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+  
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+      // updateQuery: (prev, { subscriptionData }) => {
+      //   if (!subscriptionData.data) return prev
+      //   const newVote = subscriptionData.data.newVote
+      //   console.log(newVote)
+        
+      //   const exists = prev.feed.links.find(({ id }) => id === newVote.link.id);
+      //   if (!exists) return prev;
+      //   exists.votes = newVote.link.votes;
+      //   return prev;
+      // }
+    })
+  }
+
+  _getLinksToRender = data => {
+    const isNewPage = this.props.location.pathname.includes('new')
+    if (isNewPage) {
+      return data.feed.links
+    }
+    const rankedLinks = data.feed.links.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks
+  }
+
+  _nextPage = data => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page <= data.feed.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1
+      this.props.history.push(`/new/${nextPage}`)
     }
   }
   
+  _previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page > 1) {
+      const previousPage = page - 1
+      this.props.history.push(`/new/${previousPage}`)
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-      <Query fetchPolicy="no-cache" query={FEED_QUERY}>
-        {({ loading, error, data, refetch }) => {
+      <Query query={FEED_QUERY} variables={this._getQueryVariables()}>
+        {({ loading, error, data, refetch, subscribeToMore }) => {
+        {/* {({ loading, error, data, refetch }) => { */}
           {/* var min = 1;
           var max = 100;
           var rand =  parseInt(min + (Math.random() * (max-min)));
@@ -77,22 +203,41 @@ class LinkList extends Component {
           if (rand%2===0) return <div>even</div> */}
           if (loading) return <div>Fetching</div>
           if (error) return <div>error${error.message}</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
+
+          const linksToRender = this._getLinksToRender(data)
+          const isNewPage = this.props.location.pathname.includes('new')
+          const pageIndex = this.props.match.params.page
+            ? (this.props.match.params.page - 1) * LINKS_PER_PAGE
+            : 0
+
           this.refetch = refetch;
-          const linksToRender = data.feed.links
           console.log("query")
           // <Query query={FEED_QUERY}>
           // ...
           return (
-            <div>
+            <Fragment>
               {linksToRender.map((link, index) => (
-                <Link 
-                  key={link.id} 
-                  link={link} 
-                  index={index} 
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index + pageIndex}
                   updateStoreAfterVote={this._updateCacheAfterVote}
                 />
               ))}
-            </div>
+              {isNewPage && (
+                <div className="flex ml4 mv3 gray">
+                  <div className="pointer mr2" onClick={this._previousPage}>
+                    Previous
+                  </div>
+                  <div className="pointer" onClick={() => this._nextPage(data)}>
+                    Next
+                  </div>
+                </div>
+              )}
+            </Fragment>
           )
           // ...
           // </Query>
